5a8a0b14302bd8789c7e44e6ce15f3ade4186ca3
jose luis <jose1914luis@gmail.com>
2018-01-11 14:41:03 -0500

    add user 3

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,34 +1,56 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { ErrorHandler, NgModule } from '@angular/core';
-import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
+import {BrowserModule} from '@angular/platform-browser';
+import {ErrorHandler, NgModule} from '@angular/core';
+import {IonicApp, IonicErrorHandler, IonicModule} from 'ionic-angular';
 
-import { MyApp } from './app.component';
-import { HomePage } from '../pages/home/home';
-import { ListPage } from '../pages/list/list';
+import {MyApp} from './app.component';
+import {HomePage} from '../pages/home/home';
+import {ListPage} from '../pages/list/list';
+import {AsignarPage} from '../pages/asignar/asignar';
+import {CiudadPage} from '../pages/ciudad/ciudad';
+import {ClientesPage} from '../pages/clientes/clientes';
+import {NomPage} from '../pages/nom/nom';
+import {PagoPage} from '../pages/pago/pago';
+import {SalarioPage} from '../pages/salario/salario';
+import {SolPage} from '../pages/sol/sol';
 
-import { StatusBar } from '@ionic-native/status-bar';
-import { SplashScreen } from '@ionic-native/splash-screen';
+
+import {StatusBar} from '@ionic-native/status-bar';
+import {SplashScreen} from '@ionic-native/splash-screen';
 
 @NgModule({
-  declarations: [
-    MyApp,
-    HomePage,
-    ListPage
-  ],
-  imports: [
-    BrowserModule,
-    IonicModule.forRoot(MyApp),
-  ],
-  bootstrap: [IonicApp],
-  entryComponents: [
-    MyApp,
-    HomePage,
-    ListPage
-  ],
-  providers: [
-    StatusBar,
-    SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
-  ]
+    declarations: [
+        MyApp,
+        HomePage,
+        ListPage,
+        AsignarPage,
+        CiudadPage,
+        ClientesPage,
+        NomPage,
+        PagoPage,
+        SalarioPage,
+        SolPage
+    ],
+    imports: [
+        BrowserModule,
+        IonicModule.forRoot(MyApp),
+    ],
+    bootstrap: [IonicApp],
+    entryComponents: [
+        MyApp,
+        HomePage,
+        ListPage,
+        AsignarPage,
+        CiudadPage,
+        ClientesPage,
+        NomPage,
+        PagoPage,
+        SalarioPage,
+        SolPage
+    ],
+    providers: [
+        StatusBar,
+        SplashScreen,
+        {provide: ErrorHandler, useClass: IonicErrorHandler}
+    ]
 })
 export class AppModule {}
