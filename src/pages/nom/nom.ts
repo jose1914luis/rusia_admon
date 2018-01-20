7dc42edbea6ad3b989f7217ecc575d6e917618bd
jose luis <jose1914luis@gmail.com>
2018-01-20 08:42:26 -0500

    nuevos avances 5

--- src/pages/nom/nom.ts
+++ src/pages/nom/nom.ts
@@ -15,7 +15,13 @@ export class NomPage {
     cargar = true;
     constructor(public navCtrl: NavController, public navParams: NavParams, public alertCtrl: AlertController) {
 
+        
+    }
+
+    ionViewDidLoad() {
         var self = this;
+        self.cargar = true;
+        self.items = null;
         var odoo = new OdooApi(global.url, global.db);
         odoo.login(global.username, global.password).then(
             function (uid) {
@@ -39,10 +45,6 @@ export class NomPage {
         );
     }
 
-    ionViewDidLoad() {
-        console.log('ionViewDidLoad CiudadPage');
-    }
-
     presentAlert(titulo, texto) {
         const alert = this.alertCtrl.create({
             title: titulo,
@@ -55,6 +57,66 @@ export class NomPage {
     ejecute(item) {
 //        console.log(NomDetailPage);
         this.navCtrl.push(NomDetailPage, {item: item});
+    }
+    
+    refresh() {
+        this.ionViewDidLoad();
+    }
+
+    buscar() {
+
+        var self = this;
+        let alert = this.alertCtrl.create({
+            title: 'Buscar',
+            inputs: [
+                {
+                    name: 'semIni',
+                    placeholder: 'Semana Inicial'
+                },
+                {
+                    name: 'semFin',
+                    placeholder: 'Semana Final'
+                },
+                {
+                    type: 'radio',
+                    label: 'todos',
+                    value: 'todos'
+                }
+            ],
+            buttons: [
+                {
+                    text: 'Cancel',
+                    role: 'cancel',
+                    handler: data => {
+                        console.log('Cancel clicked');
+                    }
+                },
+                {
+                    text: 'Buscar',
+                    handler: data => {
+//                        
+//                        if (data.semIni.length > 0 || data.semFin.length > 0 || data.fechaIni.length > 0 || data.fechaFin.length > 0) {
+//                            for (let key in self.items) {
+//
+//                                self.items[key].visible = false;
+//                                
+//                                if (self.items[key].semana >= data.semIni && self.items[key].semana <= data.semFin) {
+////                                    console.log(self.items[key]);
+//                                    self.items[key].visible = true;
+//                                }
+//                            }
+//                        } else {
+//                            for (let key in self.items) {
+//
+//                                self.items[key].visible = true;
+//                            }
+//                        }
+
+                    }
+                }
+            ]
+        });
+        alert.present();
     }
 }
 
--- src/pages/pago/pago.ts
+++ src/pages/pago/pago.ts
@@ -15,41 +15,38 @@ export class PagoPage {
     cargar = true;
     constructor(public navCtrl: NavController, public navParams: NavParams, public alertCtrl: AlertController) {
 
+    }
+
+    ionViewDidLoad() {
         var self = this;
+        self.cargar = true;
+        self.items = null;
         var odoo = new OdooApi(global.url, global.db);
         odoo.login(global.username, global.password).then(
             function (uid) {
-                //                odoo.search_read('tours.clientes.email', [['id', '!=', '0']], ['name', 'ilike']).then(
-                //                    function (value) {
-                //                        console.log(value);
                 odoo.search_read('tours.pago.guia', [['id', '!=', '0']],
                     ['name', 'semana', 'tours_id', 'guia_user_id', 'city_id',
                         'total_eur', 'total_usd', 'total_res', 'total_rub', 'total_metro', 'pax_pago', 'state', 'concepto']).then(
                     function (value2) {
                         console.log(value2);
                         self.items = value2
+
+                        for (let key in self.items) {
+
+                            self.items[key].visible = true;
+                        }
                         self.cargar = false;
                     },
                     function () {
                         self.presentAlert('Falla', 'Imposible Conectar');
                     }
                     );
-                //self.items = valuetours.clientes
-                //                    },
-                //                    function () {
-                //                        self.presentAlert('Falla', 'Imposible Conectar');
-                //                    }
-                //                );
 
             },
             function () {
 
             }
         );
-    }
-
-    ionViewDidLoad() {
-        console.log('ionViewDidLoad CiudadPage');
     }
 
     presentAlert(titulo, texto) {
@@ -64,6 +61,61 @@ export class PagoPage {
     ejecute(item) {
 
         this.navCtrl.push(PagoDetailPage, {item: item});
+    }
+
+    refresh() {
+        this.ionViewDidLoad();
+    }
+
+    buscar() {
+
+        var self = this;
+        let alert = this.alertCtrl.create({
+            title: 'Buscar',
+            inputs: [
+                {
+                    name: 'semIni',
+                    placeholder: 'Semana Inicial'
+                },
+                {
+                    name: 'semFin',
+                    placeholder: 'Semana Final'
+                }
+            ],
+            buttons: [
+                {
+                    text: 'Cancel',
+                    role: 'cancel',
+                    handler: data => {
+                        console.log('Cancel clicked');
+                    }
+                },
+                {
+                    text: 'Buscar',
+                    handler: data => {
+                        
+                        if (data.semIni.length > 0 || data.semFin.length > 0 || data.fechaIni.length > 0 || data.fechaFin.length > 0) {
+                            for (let key in self.items) {
+
+                                self.items[key].visible = false;
+                                
+                                if (self.items[key].semana >= data.semIni && self.items[key].semana <= data.semFin) {
+//                                    console.log(self.items[key]);
+                                    self.items[key].visible = true;
+                                }
+                            }
+                        } else {
+                            for (let key in self.items) {
+
+                                self.items[key].visible = true;
+                            }
+                        }
+
+                    }
+                }
+            ]
+        });
+        alert.present();
     }
 }
 
