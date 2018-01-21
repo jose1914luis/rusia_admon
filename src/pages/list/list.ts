f96c9866016b9c78d407d244cdfee3f5acb4ff95
jose luis <jose1914luis@gmail.com>
2018-01-21 11:14:47 -0500

    nuevos avances 11

--- src/pages/list/list.ts
+++ src/pages/list/list.ts
@@ -15,6 +15,7 @@ export class ListPage {
     cargar = true;
     mensaje = '';
     conexion = global;
+    bd = 'Free_Tour_Russia';
     constructor(public navCtrl: NavController, public navParams: NavParams, private storage: Storage, public alertCtrl: AlertController) {
 
         var borrar = this.navParams.get('borrar');
