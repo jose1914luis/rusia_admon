d3a897c3007caa299727cb5a67753504023380c8
jose luis <jose1914luis@gmail.com>
2018-01-21 11:02:39 -0500

    nuevos avances 11

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,60 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import {Component} from '@angular/core';
+import {NavController, AlertController} from 'ionic-angular';
+import {global} from '../../components/credenciales/credenciales';
 
+declare var OdooApi: any;
 @Component({
-  selector: 'page-home',
-  templateUrl: 'home.html'
+    selector: 'page-home',
+    templateUrl: 'home.html'
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+    items;
+    cargar;
+    mensaje = '';
+    constructor(public navCtrl: NavController, public alertCtrl: AlertController) {
+    }
 
-  }
+    ionViewDidLoad() {
+        this.cargar = true
+        var self = this;
+        this.items = null;
+        var odoo = new OdooApi(global.url, global.db);
+
+        odoo.login(global.username, global.password).then(
+            function (uid) {
+                odoo.search_read('tours.gastos.diversos', [['id', '!=', '0']],
+                    ['name', 'city_id', 'total_usd', 'total_eur', 'total_rub', 'total_pp', 'total_tarjeta',
+                    'state', 'conceptos_ids']).then(
+                    function (value2) {
+                        console.log(value2);
+                        self.items = value2
+                        self.cargar = false;
+                    },
+                    function () {
+                        self.presentAlert('Falla', 'Imposible Conectar');
+                    }
+                    );
+
+            },
+            function () {
+
+            }
+        );
+    }
+
+
+    presentAlert(titulo, texto) {
+        const alert = this.alertCtrl.create({
+            title: titulo,
+            subTitle: texto,
+            buttons: ['Ok']
+        });
+        alert.present();
+    }
+
+    refresh() {
+        this.ionViewDidLoad();
+    }
 
 }
--- src/pages/sol/sol.ts
+++ src/pages/sol/sol.ts
@@ -1,24 +1,60 @@
-import { Component } from '@angular/core';
-import { NavController, NavParams } from 'ionic-angular';
+import {Component} from '@angular/core';
+import {NavController, NavParams, AlertController} from 'ionic-angular';
+import {global} from '../../components/credenciales/credenciales';
 
-/**
- * Generated class for the SolPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
+declare var OdooApi: any;
 
 @Component({
-  selector: 'page-sol',
-  templateUrl: 'sol.html',
+    selector: 'page-sol',
+    templateUrl: 'sol.html',
 })
 export class SolPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
-  }
+    items;
+    cargar;
+    mensaje = '';
+    constructor(public navCtrl: NavController, public navParams: NavParams, public alertCtrl: AlertController) {
+    }
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad SolPage');
-  }
+    ionViewDidLoad() {
+        this.cargar = true
+        var self = this;
+        this.items = null;
+        var odoo = new OdooApi(global.url, global.db);
+
+        odoo.login(global.username, global.password).then(
+            function (uid) {
+                odoo.search_read('tours.clientes.solicitudes', [['id', '!=', '0']],
+                    ['name', 'tour_id', 'num_person', 'date_begin', 'state', 'comentarios', 'create_date']).then(
+                    function (value2) {
+                        console.log(value2);
+                        self.items = value2
+                        self.cargar = false;
+                    },
+                    function () {
+                        self.presentAlert('Falla', 'Imposible Conectar');
+                    }
+                    );
+
+            },
+            function () {
+
+            }
+        );
+    }
+
+
+    presentAlert(titulo, texto) {
+        const alert = this.alertCtrl.create({
+            title: titulo,
+            subTitle: texto,
+            buttons: ['Ok']
+        });
+        alert.present();
+    }
+
+    refresh() {
+        this.ionViewDidLoad();
+    }
 
 }
