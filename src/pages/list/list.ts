917c2ce69b8e9ee8b00df897ddc384d7b9743e75
jose luis <jose1914luis@gmail.com>
2018-01-31 17:08:26 -0500

    nuevos avances de la app

--- src/pages/list/list.ts
+++ src/pages/list/list.ts
@@ -13,14 +13,16 @@ export class ListPage {
 
     cargar = false;
     mensaje = '';
-    conexion = {tipo_a: '', grupos_id: [], bd: 'Tour_Gratis_Rusia', username: 'toursgratissanpetersburgo@gmail.com', password: 'IaozaK9yYncv0CdSMUux', is_guia: false, is_chofer: false, is_promotor: false};
+    //toursgratissanpetersburgo@gmail.com
+    //IaozaK9yYncv0CdSMUux
+    conexion = {tipo_a: '', grupos_id: [], bd: 'Tour_Gratis_Rusia', username: '', password: '', is_guia: false, is_chofer: false, is_promotor: false};
     constructor(public navCtrl: NavController, public navParams: NavParams, private storage: Storage, public alertCtrl: AlertController) {
 
         var borrar = this.navParams.get('borrar');
         //        this.storage.get('conexion').then((conexion) => {
         //            console.log(conexion); 
         //        });
-        this.storage.remove('conexion');
+        //this.storage.remove('conexion');
         if (borrar == true) {
             this.cargar = false;
             this.storage.remove('conexion');
@@ -43,115 +45,133 @@ export class ListPage {
     conectarApp(verificar) {
 
         var self = this;
-        if (self.conexion.username.length < 5 && self.conexion.password.length < 4) return;
-        self.cargar = true;
-        var odoo = new OdooApi(global.url, self.conexion.bd);
-        odoo.login(self.conexion.username, self.conexion.password).then(
-            function (uid) {
-                console.log(uid);
-                odoo.search_read('tours', [['id', '<>', '0']], ['name']).then(
-                    function (tours) {
-                        //                        console.log(tours);
-                        self.storage.set('tours', tours)//<--- lista de los tours 
-                        odoo.read('res.users', [uid],
-                            ['name', 'email', 'city_id', 'is_guia', 'is_chofer', 'is_promotor', 'salario_ext', 'salario_min',
-                                'active', 'groups_id']).then(
-                            function (users) {
-                                console.log(users);
-                                self.conexion.is_chofer = users[0].is_chofer;
-                                self.conexion.is_guia = users[0].is_guia;
-                                self.conexion.is_promotor = users[0].is_promotor;
-                                if (!users[0].is_chofer && !users[0].is_guia && !users[0].is_promotor) {
-                                    console.log('entro al filtro')
-                                    for (let key in users[0].groups_id) {
-                                        //si es 12=administrador, 14=administrador x ciudad, 15 gerente
-                                        if (users[0].groups_id[key] == 15 || users[0].groups_id[key] == 14) {
-                                            self.conexion.tipo_a = 'xciudad'
-                                            console.log('entro al filtro')
-                                        }
+        
+        this.storage.get('conexion').then((val) => {
 
-                                    }
-                                }
+            if (val == null) {//no existe datos         
+                self.cargar = false;
+                //                    con = self.CONEXION;
+                if (self.conexion.username.length < 3 || self.conexion.password.length < 3) {
 
-                                //self.conexion.grupos_id  = ;
-                                self.storage.set('conexion', self.conexion);//<--- datos del usuario
-                                odoo.search_read('tours.clientes', [['id', '!=', 0]],
-                                    ['name', 'telefono', 'nombre_hotel', 'email', 'is_padrino', 'active_email', 'pago_tarjeta', 'padre', 'observaciones']).then(
-                                    function (clientes) {
-                                        console.log(clientes)
-                                        self.storage.set('clientes', clientes);//<--Todos los clientes Clientes  
-
-                                        odoo.search_read('tours.clientes.email', [['id', '!=', 0]],
-                                            ['name']).then(
-                                            function (email) {
-                                                console.log(email)
-                                                self.storage.set('email', email);//<--Todos los emails
-                                                var consulta;
-                                                if (self.conexion.tipo_a == 'xciudad') {
-                                                    consulta = [['administrador', '=', uid]]
-                                                } else {
-                                                    consulta = [['id', '!=', '0']]
-                                                }
-                                                odoo.search_read('tours.companies', consulta, ['name', 'administrador']).then(
-                                                    function (companies) {
-                                                        console.log(companies);
-                                                        self.storage.set('companies', companies); //<--- Todas las Ciudades
-                                                        var ban = true;
-
-                                                        for (var key = 0; companies.length > key; key++) {
-
-                                                            (function (key) {
-                                                                console.log(key);
-
-                                                                if (companies[key].administrador[0] == uid) {
-                                                                    odoo.search_read('res.users', [['city_id', '=', companies[key].name[0]], ['active', '=', 1]],
-                                                                        ['name']).then(
-                                                                        function (guias) {
-                                                                            console.log(guias)
-                                                                            self.storage.set('guias', guias);//<--Guias si los hay                      
-                                                                        }, function () {
-                                                                            self.loginSinDatos();
-                                                                        }
-                                                                        )
-                                                                }
-                                                            })(key);
-                                                        }
-
-                                                        self.navCtrl.setRoot(PanelPage);
-
-                                                    },
-                                                    function () {
-                                                        self.loginSinDatos();
-                                                    });
-
-                                            }, function () {
-                                                self.loginSinDatos();
-                                            }
-                                            )
-
-                                    }, function () {
-                                        self.loginSinDatos();
-                                    }
-                                    )
-
-
-                            },
-                            function () {
-                                self.loginSinDatos();
-                            }
-                            );
-                    },
-                    function () {
-                        self.loginSinDatos();
+                    if (verificar) {
+                        self.presentAlert('Alerta!', 'Por favor ingrese usuario y contraseña');
                     }
-                );
+                    return;
+                }
 
-            },
-            function () {
-                self.loginSinDatos()
+            } else {
+                //si los trae directamente ya fueron verificados
+                self.conexion = val;
             }
-        );
+            self.cargar = true;
+            var odoo = new OdooApi(global.url, self.conexion.bd);
+            odoo.login(self.conexion.username, self.conexion.password).then(
+                function (uid) {
+                    console.log(uid);
+                    odoo.search_read('tours', [['id', '<>', '0']], ['name']).then(
+                        function (tours) {
+                            //                        console.log(tours);
+                            self.storage.set('tours', tours)//<--- lista de los tours 
+                            odoo.read('res.users', [uid],
+                                ['name', 'email', 'city_id', 'is_guia', 'is_chofer', 'is_promotor', 'salario_ext', 'salario_min',
+                                    'active', 'groups_id']).then(
+                                function (users) {
+                                    console.log(users);
+                                    self.conexion.is_chofer = users[0].is_chofer;
+                                    self.conexion.is_guia = users[0].is_guia;
+                                    self.conexion.is_promotor = users[0].is_promotor;
+                                    if (!users[0].is_chofer && !users[0].is_guia && !users[0].is_promotor) {
+                                        console.log('entro al filtro')
+                                        for (let key in users[0].groups_id) {
+                                            //si es 12=administrador, 14=administrador x ciudad, 15 gerente
+                                            if (users[0].groups_id[key] == 15 || users[0].groups_id[key] == 14) {
+                                                self.conexion.tipo_a = 'xciudad'
+                                                console.log('entro al filtro')
+                                            }
 
+                                        }
+                                    }
+
+                                    //self.conexion.grupos_id  = ;
+                                    self.storage.set('conexion', self.conexion);//<--- datos del usuario
+                                    odoo.search_read('tours.clientes', [['id', '!=', 0]],
+                                        ['name', 'telefono', 'nombre_hotel', 'email', 'is_padrino', 'active_email', 'pago_tarjeta', 'padre', 'observaciones']).then(
+                                        function (clientes) {
+                                            console.log(clientes)
+                                            self.storage.set('clientes', clientes);//<--Todos los clientes Clientes  
+
+                                            odoo.search_read('tours.clientes.email', [['id', '!=', 0]],
+                                                ['name']).then(
+                                                function (email) {
+                                                    console.log(email)
+                                                    self.storage.set('email', email);//<--Todos los emails
+                                                    var consulta;
+                                                    if (self.conexion.tipo_a == 'xciudad') {
+                                                        consulta = [['administrador', '=', uid]]
+                                                    } else {
+                                                        consulta = [['id', '!=', '0']]
+                                                    }
+                                                    odoo.search_read('tours.companies', consulta, ['name', 'administrador']).then(
+                                                        function (companies) {
+                                                            console.log(companies);
+                                                            self.storage.set('companies', companies); //<--- Todas las Ciudades
+                                                            var ban = true;
+
+                                                            for (var key = 0; companies.length > key; key++) {
+
+                                                                (function (key) {
+                                                                    console.log(key);
+
+                                                                    if (companies[key].administrador[0] == uid) {
+                                                                        odoo.search_read('res.users', [['city_id', '=', companies[key].name[0]], ['active', '=', 1]],
+                                                                            ['name']).then(
+                                                                            function (guias) {
+                                                                                console.log(guias)
+                                                                                self.storage.set('guias', guias);//<--Guias si los hay                      
+                                                                            }, function () {
+                                                                                self.loginSinDatos();
+                                                                            }
+                                                                            )
+                                                                    }
+                                                                })(key);
+                                                            }
+
+                                                            self.navCtrl.setRoot(PanelPage);
+
+                                                        },
+                                                        function () {
+                                                            self.loginSinDatos();
+                                                        });
+
+                                                }, function () {
+                                                    self.loginSinDatos();
+                                                }
+                                                )
+
+                                        }, function () {
+                                            self.loginSinDatos();
+                                        }
+                                        )
+
+
+                                },
+                                function () {
+                                    self.loginSinDatos();
+                                }
+                                );
+                        },
+                        function () {
+                            self.loginSinDatos();
+                        }
+                    );
+
+                },
+                function () {
+                    self.loginSinDatos()
+                }
+            );
+
+        });
     }
 
     presentAlert(titulo, texto) {
